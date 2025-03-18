2fab685f2c52c309baad6fb0f5a163f2f48e8d7f
Asiul Rosillo <zeazeta@gmail.com>
2025-03-18 15:16:35 -0600

    Declaración de Variables Globales:

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// app.js
+
+// Array para almacenar los nombres de los participantes del amigo secreto (vacio).
+const listaDeAmigos =``;
+
+// Obtener referencias a elementos del DOM para interactuar con el HTML.
+const inputAmigo = document.getElementById('amigo'); // Campo de texto para ingresar nombres.
+const listaAmigosElement = document.getElementById('listaAmigos'); // Lista (<ul>) donde se mostrarán los nombres agregados.
+const resultadoElement = document.getElementById('resultado'); // Lista (<ul>) donde se mostrarán los resultados del sorteo.
